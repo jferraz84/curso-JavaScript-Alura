99adfd43e32aa009487ec176a6c0163dae2f559b
Jorge Ferraz <jferrazjorge@gmail.com>
2020-04-21 16:13:34 -0300

    Aula 2 - Variáveis e Operadores

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,39 @@
-console.log("Fui carregado de um aqruivo de uma arquivo externo");
 
 var titulo = document.querySelector(".titulo");
-            
-titulo.textContent = "Aparecida Nutricionista"
-            
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 500) {
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválido");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválido";
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+    
+} 
+
+
+
+
+
+

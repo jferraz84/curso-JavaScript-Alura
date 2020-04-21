d026a2c5342306e48b5e98267acc5d2dc3eb837d
Jorge Ferraz <jferrazjorge@gmail.com>
2020-04-21 14:31:24 -0300

    Aula 1 - Introdução

---
+++ js/principal.js
@@ -0,0 +1,6 @@
+console.log("Fui carregado de um aqruivo de uma arquivo externo");
+
+var titulo = document.querySelector(".titulo");
+            
+titulo.textContent = "Aparecida Nutricionista"
+            
